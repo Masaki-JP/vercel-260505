18447e0cbb69598d60d9fee36f842838118bba62
Masaki-JP <masakidayo0508@gmail.com>
2026-05-06 11:43:49 +0900

    skip

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return <main>
-    <h1 className="text-3xl font-bold bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Welcome to My App</h1>
+    <h1 className="text-3xl font-bold bg-linear-to-r from-red-500 to-purple-500 bg-clip-text text-transparent">Welcome to My App</h1>
   </main>;
 }
