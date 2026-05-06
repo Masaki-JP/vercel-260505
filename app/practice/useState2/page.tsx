d355bb1b184372a5eb1c371476a12539134cced6
Masaki-JP <masakidayo0508@gmail.com>
2026-05-07 01:17:13 +0900

    Expand useState2 practice page

--- app/practice/useState2/page.tsx
+++ app/practice/useState2/page.tsx
@@ -1,5 +1,35 @@
+"use client"
+
+import { useState } from "react"
+
 export default function UseState2() {
-    return <p>
-        UseState2
-    </p>
+    const [person, setPerson] = useState({
+        name: "Naruto",
+        age: 15
+    })
+
+    return <main className="max-w-5xl mx-auto text-7xl mt-8 flex flex-col items-center space-y-8">
+        <p>Name: {person.name}</p>
+        <p>Age: {person.age}</p>
+
+        <button onClick={() => {
+            setPerson((pre) => {
+                return {
+                ...person,
+                age: pre.age + 1
+            }
+            })
+        }}>
+            年齢を１加算
+        </button>
+
+        <button onClick={() => {
+            setPerson((pre) => {
+                pre.age++
+                return pre
+            })
+        }}>
+            年齢を１加算<br />（実際に加算されるが再描画されない）
+        </button>
+    </main>
 }
